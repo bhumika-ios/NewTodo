c08c1c8c5e4b071ef212b62c145d509dc535af67
bhumika-ios <bhumika.iosdeveloper@gmail.com>
2026-09-23 16:39:19 +0530

    Initial Commit

---
+++ NewTodo/NewTodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewTodoApp.swift
+//  NewTodo
+//
+//  Created by Bhumika Patel on 23/09/26.
+//
+
+import SwiftUI
+
+@main
+struct NewTodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
